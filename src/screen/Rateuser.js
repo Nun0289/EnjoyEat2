288f59d4448bbfca3bb2cad289d8fd59efba3cf6
Nuntavat suthangkornkul <42973803+Nun0289@users.noreply.github.com>
2020-11-25 05:46:41 +0700

    .

--- src/screen/Rateuser.js
+++ src/screen/Rateuser.js
@@ -7,11 +7,17 @@ import {
   View,
   FlatList,
   TouchableOpacity,
+  Modal,
+  TextInput,
 } from "react-native";
+import { Dimensions } from "react-native";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { CommonActions } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
 import StarRating from "react-native-star-rating";
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 const DATA = [
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
@@ -33,8 +39,107 @@ const DATA = [
 const Rateuser = ({ navigation }) => {
   const [starCount, onStarRatingPress] = useState(0);
   const [addFriends, setAddFriends] = useState(true);
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View style={styles.container}>
+      <Modal animationType="slide" visible={modalVisible} transparent={true}>
+        <TouchableOpacity
+          style={{ flex: 1 }}
+          onPress={() => setModalVisible(!modalVisible)}
+        >
+          <View
+            style={{
+              flex: 1,
+              alignItems: "center",
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+            }}
+          >
+            <LinearGradient
+              // Button Linear Gradient
+              colors={["#D989FF", "#687FFE", "#21D4FD"]}
+              style={styles.modalView}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                }}
+              >
+                <View
+                  style={{
+                    width: 150,
+                    height: 150,
+                    borderRadius: 100,
+                    borderColor: "#FFF",
+                    borderWidth: 10,
+                    position: "absolute",
+                    top: -70,
+                    shadowColor: "#000",
+                    shadowOffset: {
+                      width: 0,
+                      height: 7,
+                    },
+                    shadowOpacity: 0.43,
+                    shadowRadius: 9.51,
+
+                    elevation: 15,
+                  }}
+                >
+                  <Image
+                    style={{ flex: 1, borderRadius: 100 }}
+                    source={{
+                      uri: "https://reactnative.dev/img/tiny_logo.png",
+                    }}
+                  />
+                </View>
+                <View
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <View style={{ paddingTop: 100, flexDirection: "row" }}>
+                    <Text style={{ fontSize: 30, color: "#FFF" }}>User1</Text>
+                  </View>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      paddingRight: 200,
+                    }}
+                  >
+                    <Text style={{ fontSize: 18, paddingRight: 5 }}>4.5</Text>
+                    <FontAwesome name="star" size={25} color="yellow" />
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        position: "absolute",
+                        right: 10,
+                      }}
+                    >
+                      <FontAwesome name="user" size={25} color="yellow" />
+                    </View>
+                  </View>
+                  <View
+                    style={{
+                      marginTop: 20,
+                      width: "80%",
+                      height: "100%",
+                      borderRadius: 20,
+                      backgroundColor: "#FFF",
+                      padding: 20,
+                    }}
+                  >
+                    <Text style={{ paddingBottom: 20 }}>Name:</Text>
+                    <Text style={{ paddingBottom: 20 }}>Address:</Text>
+                  </View>
+                </View>
+              </View>
+            </LinearGradient>
+          </View>
+        </TouchableOpacity>
+      </Modal>
       <FlatList
         data={DATA}
         renderItem={({ item, index }) => {
@@ -47,13 +152,18 @@ const Rateuser = ({ navigation }) => {
                   justifyContent: "center",
                 }}
               >
-                <Image
-                  style={{ width: 60, height: 60, borderRadius: 100 }}
-                  source={{
-                    uri: "https://reactnative.dev/img/tiny_logo.png",
-                  }}
-                />
+                <TouchableOpacity
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Image
+                    style={{ width: 60, height: 60, borderRadius: 100 }}
+                    source={{
+                      uri: "https://reactnative.dev/img/tiny_logo.png",
+                    }}
+                  />
+                </TouchableOpacity>
               </View>
+
               <View style={{ flex: 3 }}>
                 <View style={{ flexDirection: "column", flex: 1 }}>
                   <View
@@ -185,6 +295,87 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 32,
+  },
+
+  yellowblock: {
+    width: "100%",
+    height: windowHeight / 2,
+    backgroundColor: "#FDD009",
+    position: "absolute",
+    top: 0,
+    borderRadius: 30,
+  },
+  loginbox: {
+    width: "80%",
+    height: windowHeight / 2,
+    backgroundColor: "#FFF",
+    borderRadius: 20,
+    position: "absolute",
+    flexDirection: "column",
+    bottom: "12%",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    shadowOpacity: 0.44,
+    shadowRadius: 10.32,
+
+    elevation: 16,
+  },
+  rowlogin: {
+    flex: 1,
+    paddingTop: 20,
+  },
+  TextInput: {
+    height: 40,
+    width: "90%",
+    borderRadius: 10,
+    borderColor: "gray",
+    borderWidth: 1,
+    backgroundColor: "#F5F5F5",
+    paddingLeft: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  modalView: {
+    backgroundColor: "#EFF1FC",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    flexDirection: "column",
+    width: "90%",
+    height: "65%",
+    position: "absolute",
+    bottom: 0,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    textAlign: "center",
   },
 });
 
